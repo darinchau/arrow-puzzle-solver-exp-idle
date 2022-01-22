8d0ee7c34d58c6d850952b9f89c983e07a1b4e03
vert178 <kexiluo4@gmail.com>
2022-01-22 12:43:52 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+This script solves the exponential idle hard arrow puzzle. It hasn't been very well tested yet but oh well
+
+I decided to share this script because why not.
+
+Instructions:
+
+0. Get the required packages: You need python 3.6 or above (I use 3.9)
+Pure python adb for reading and sending phone data: pip install -U pure-python-adb
+Pillow for image recognition: pip install pillow
+MEmu: the emulator of choice
+
+1. Install exponential idle on your emulator (tested on Android 7.1) and go to the hard arrow puzzle page
+
+2. Connect your emulator to the adb server by running "adb connect 127.0.0.1:21503" on your server
+
+3. Set your emulator resolution to 1200 x 800
+
+4. Set the arrow puzzle settings to greyscale and displaying numbers
+
+5. Run the script. If everything goes well it should solve one board in an average of 12 seconds.
---
+++ main.py
@@ -0,0 +1,173 @@
+# pip install -U pure-python-adb
+# pip install pillow
+from ppadb.client import Client
+from PIL import Image
+import numpy as np
+import time
+from subprocess import call
+
+i = 2865
+
+# 1. Connect to device
+client = Client(host='127.0.0.1', port=5037)
+devices = client.devices()
+
+if len(devices) == 0:
+    raise RuntimeError("No devices connected")
+
+device = devices[0]
+
+
+def take_screenshot(device):
+    image = device.screencap()
+    with open('screen.png', 'wb') as f:
+        f.write(image)
+
+
+def sendClick(imgCoords: tuple):
+    # print(f"Clicking {imgCoords}")
+    call(["adb", "shell", "input", "tap", str(imgCoords[1]), str(imgCoords[0])])
+
+
+def getState(imgCoord: tuple):
+    if (imgCoord[0] < 0):
+        return 0
+
+    pixel = Img[imgCoord[0]][imgCoord[1]]
+    # assert pixel[0] == 17 or pixel[0] == 85
+    if pixel[0] < 20:
+        return 1
+    return 2
+
+
+def getImgCoord(localCoord: tuple):
+    if not isValid(localCoord):
+        return (-1, -1)
+    return (-50 * localCoord[1] + 720, 85 * localCoord[0] + 400)
+
+
+def isValid(localCoord: tuple):
+    c = abs(localCoord[0]) + abs(localCoord[1])
+    return abs(localCoord[0]) < 4 and abs(localCoord[1]) < 7 and c < 7 and c % 2 == 0
+
+
+def getStateFromBoard(board, localCoords: tuple):
+    return board[localCoords[0] + 3][localCoords[1] + 6]
+
+
+def setStateOnBoard(board, localCoords: tuple):
+    grid = board[localCoords[0] + 3][localCoords[1] + 6]
+    grid += 1
+    if grid == 3:
+        grid = 1
+    board[localCoords[0] + 3][localCoords[1] + 6] = grid
+    return board
+
+
+def MakeMoveOn(board, localCoords: tuple):
+    # Handle the board, for itself and its 6 neighbours
+    for i in [localCoords, (localCoords[0], localCoords[1] + 2), (localCoords[0], localCoords[1] - 2), (localCoords[0] + 1, localCoords[1] + 1), (localCoords[0] + 1, localCoords[1] - 1), (localCoords[0] - 1, localCoords[1] + 1), (localCoords[0] - 1, localCoords[1] - 1)]:
+        if isValid(i):
+            board = setStateOnBoard(board, i)
+
+    # print(f"Making move on {localCoords}")
+
+    # Handle the device
+    sendClick(getImgCoord(localCoords))
+    return board
+
+# Solve a cell by clicking the one below it
+
+
+def solveBottom(board, localCoords: tuple):
+    south = (localCoords[0], localCoords[1] - 2)
+    # Check if there is a bottom cell
+    if isValid(south):
+        if getStateFromBoard(board, localCoords) == 2:
+            board = MakeMoveOn(board, south)
+    return board
+
+
+def propogate(board):
+    # Doing things the hard way :D
+    solveSequence = [(0, 6), (-1, 5), (-2, 4), (-3, 3), (1, 5), (2, 4), (3, 3),
+                     (0, 4), (-1, 3), (-2, 2), (-3, 1), (1, 3), (2, 2), (3, 1),
+                     (0, 2), (-1, 1), (-2, 0), (-3, -1), (1, 1), (2, 0), (3, -1),
+                     (0, 0), (-1, -1), (-2, -2), (1, -1), (2, -2),
+                     (0, -2), (-1, -3), (1, -3), (-0, -4)]
+
+    for i in range(len(solveSequence)):
+        board = solveBottom(board, solveSequence[i])
+
+    return board
+
+
+def solve(board):
+    board = propogate(board)
+    solver = (0, 0, 0, 0)
+    parity = (getStateFromBoard(board, (-3, -3)), getStateFromBoard(board, (-2, -4)),
+              getStateFromBoard(board, (-1, -5)), getStateFromBoard(board, (0, -6)))
+    if parity == (1, 1, 2, 1):
+        solver = (0, 1, 0, 0)
+    elif parity == (1, 2, 1, 2):
+        solver = (0, 1, 1, 0)
+    elif parity == (1, 2, 2, 2):
+        solver = (0, 0, 1, 0)
+    elif parity == (2, 1, 1, 2):
+        solver = (1, 0, 0, 0)
+    elif parity == (2, 1, 2, 2):
+        solver = (0, 0, 0, 1)
+    elif parity == (2, 2, 1, 1):
+        solver = (0, 0, 1, 1)
+    elif parity == (2, 2, 2, 1):
+        solver = (1, 0, 1, 0)
+
+    if solver[0]:
+        board = MakeMoveOn(board, (-3, 3))
+    if solver[1]:
+        board = MakeMoveOn(board, (-2, 4))
+    if solver[2]:
+        board = MakeMoveOn(board, (-1, 5))
+    if solver[3]:
+        board = MakeMoveOn(board, (0, 6))
+
+    board = propogate(board)
+    return board
+
+
+def testClicks():
+    for x in range(-3, 4):
+        for y in range(-6, 7):
+            if isValid((x, y)):
+                print(f"Clicking {(x,y)}")
+                imgCoord = getImgCoord((x, y))
+                sendClick(imgCoord)
+                Img[imgCoord[0]][imgCoord[1]] = [255, 0, 0, 255]
+
+    image = Image.fromarray(Img)
+    image = image.save("screen.png")
+
+while True:
+    # 2. Get image
+    take_screenshot(device)
+    image = Image.open('screen.png')
+    Img = np.asarray(image)
+
+    resolution = (len(Img), len(Img[0]))
+    # print(resolution)
+
+    # 3. Get board state and store it into an array
+    board = []
+    for x in range(-3, 4):
+        arr = []
+        for y in range(-6, 7):
+            arr.append(getState(getImgCoord((x, y))))
+        board.append(arr)
+
+    board = solve(board)
+    sendClick((1120, 360))
+    time.sleep(0.5)
+    sendClick((69, 648))
+    i += 1
+    print("solved " + str(i) + " boards")
+    time.sleep(0.5)
