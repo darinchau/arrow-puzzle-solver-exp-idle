0f16e605bfc2cb73e4493d1c22918a46048f0cbb
vert178 <kexiluo4@gmail.com>
2022-02-13 12:51:08 +0800

    Adding UI

---
+++ solver.py
@@ -0,0 +1,201 @@
+# pip install -U pure-python-adb
+# pip install pillow
+# pip install pyautogui
+from ppadb.client import Client
+from PIL import Image
+import numpy as np
+import time
+import pyautogui
+from subprocess import call
+from datetime import datetime
+import statistics
+import PySimpleGUI as sg
+
+
+def initialize():
+    # 1. Connect to device
+    client = Client(host='127.0.0.1', port=5037)
+    devices = client.devices()
+
+    if len(devices) == 0:
+        raise RuntimeError("No devices connected")
+
+    device = devices[0]
+    time.sleep(2)
+    return device
+
+
+def take_screenshot(device, imagename):
+    image = device.screencap()
+    with open(imagename, 'wb') as f:
+        f.write(image)
+
+
+def sendClick(localCoord: tuple):
+    if True:
+        imgCoords = (85 * localCoord[0] + 400, -50 * localCoord[1] + 720)
+        call(["adb", "shell", "input", "tap",
+             f"{imgCoords[0]}", f"{imgCoords[1]}"])
+    else:
+        imgCoords = (120 * localCoord[0] + 1333, -70 * localCoord[1] + 1060)
+        pyautogui.leftClick(imgCoords[0], imgCoords[1])
+
+
+def getState(Img, imgCoord: tuple):
+    if (imgCoord[0] < 0):
+        return 0
+
+    pixel = Img[imgCoord[1]][imgCoord[0]]
+    # assert pixel[0] == 17 or pixel[0] == 85
+    if pixel[0] < 20:
+        return 1
+    return 2
+
+
+def getImgCoord(localCoord: tuple):
+    if not isValid(localCoord):
+        return (-1, -1)
+    return (85 * localCoord[0] + 400, -50 * localCoord[1] + 720)
+
+
+def isValid(localCoord: tuple):
+    c = abs(localCoord[0]) + abs(localCoord[1])
+    return abs(localCoord[0]) < 4 and abs(localCoord[1]) < 7 and c < 7 and c % 2 == 0
+
+
+def getStateFromBoard(board, localCoords: tuple):
+    return board[localCoords[0] + 3][localCoords[1] + 6]
+
+
+def setStateOnBoard(board, localCoords: tuple):
+    grid = board[localCoords[0] + 3][localCoords[1] + 6]
+    grid += 1
+    if grid == 3:
+        grid = 1
+    board[localCoords[0] + 3][localCoords[1] + 6] = grid
+    return board
+
+
+def MakeMoveOn(board, moves: list, localCoords: tuple, click=True):
+    # Handle the board, for itself and its 6 neighbours
+    for i in [localCoords, (localCoords[0], localCoords[1] + 2), (localCoords[0], localCoords[1] - 2), (localCoords[0] + 1, localCoords[1] + 1), (localCoords[0] + 1, localCoords[1] - 1), (localCoords[0] - 1, localCoords[1] + 1), (localCoords[0] - 1, localCoords[1] - 1)]:
+        if isValid(i):
+            board = setStateOnBoard(board, i)
+    if click:
+        moves.append(localCoords)
+    return board
+
+
+def solveBottom(board, moves: list, localCoords: tuple, click=True):
+    south = (localCoords[0], localCoords[1] - 2)
+    # Check if there is a bottom cell
+    if isValid(south):
+        if getStateFromBoard(board, localCoords) == 2:
+            board = MakeMoveOn(board, moves, south, click)
+    return board
+
+
+def copyBoard(board):
+    copy = []
+    for i in range(len(board)):
+        arr = []
+        for j in range(len(board[0])):
+            arr.append(board[i][j])
+        copy.append(arr)
+    return copy
+
+
+def propogate(board, moves: list, click=True):
+    # Doing things the hard way :D
+    solveSequence = [(0, 6), (-1, 5), (-2, 4), (-3, 3), (1, 5), (2, 4), (3, 3),
+                     (0, 4), (-1, 3), (-2, 2), (-3, 1), (1, 3), (2, 2), (3, 1),
+                     (0, 2), (-1, 1), (-2, 0), (-3, -1), (1, 1), (2, 0), (3, -1),
+                     (0, 0), (-1, -1), (-2, -2), (1, -1), (2, -2),
+                     (0, -2), (-1, -3), (1, -3), (-0, -4)]
+
+    for i in range(len(solveSequence)):
+        board = solveBottom(board, moves, solveSequence[i], click)
+
+    return board, moves
+
+
+def solve(board, moves: list):
+    copy = copyBoard(board)
+    copy, moves = propogate(copy, moves, False)
+    solver = (0, 0, 0, 0)
+    parity = (getStateFromBoard(copy, (-3, -3)), getStateFromBoard(copy, (-2, -4)),
+              getStateFromBoard(copy, (-1, -5)), getStateFromBoard(copy, (0, -6)))
+    if parity == (1, 1, 2, 1):
+        solver = (0, 1, 0, 0)
+    elif parity == (1, 2, 1, 2):
+        solver = (0, 1, 1, 0)
+    elif parity == (1, 2, 2, 2):
+        solver = (0, 0, 1, 0)
+    elif parity == (2, 1, 1, 2):
+        solver = (1, 0, 0, 0)
+    elif parity == (2, 1, 2, 2):
+        solver = (0, 0, 0, 1)
+    elif parity == (2, 2, 1, 1):
+        solver = (0, 0, 1, 1)
+    elif parity == (2, 2, 2, 1):
+        solver = (1, 0, 1, 0)
+
+    if solver[0]:
+        board = MakeMoveOn(board, moves, (-3, 3))
+    if solver[1]:
+        board = MakeMoveOn(board, moves, (-2, 4))
+    if solver[2]:
+        board = MakeMoveOn(board, moves, (-1, 5))
+    if solver[3]:
+        board = MakeMoveOn(board, moves, (0, 6))
+        
+    board, moves = propogate(board, moves)
+    return moves
+
+
+def getStats(t):
+    nrsolve = len(t)
+    avg = sum(t)/nrsolve
+    stdev = statistics.stdev(t) if nrsolve > 2 else 0
+    minimum = min(t)
+    return nrsolve, avg, stdev, minimum
+
+
+def solveBoard(device, t):
+    # 2. Get image
+    take_screenshot(device, 'screen.png')
+    image = Image.open('screen.png')
+    Img = np.asarray(image)
+
+    # 3. Get board state and store it into an array
+    board = []
+    for x in range(-3, 4):
+        arr = []
+        for y in range(-6, 7):
+            arr.append(getState(Img, getImgCoord((x, y))))
+        board.append(arr)
+
+    # 4. Solve board and do bookkeeping
+    moves = solve(board, [])
+    t1 = datetime.now()
+    for i in moves:
+        sendClick(i)
+    t2 = datetime.now()
+
+    # 5. Reset and error check
+    sendClick((0, -8))
+
+    solveTime = round((t2 - t1).total_seconds(), 3)
+    t.append((t2 - t1).total_seconds())
+    nrSolve, avg, std, m = getStats(t)
+
+    if solveTime <= 1.800:
+        take_screenshot(device, 'screen' + str(nrSolve) + '.png')
+
+    time.sleep(0.3)
+    sendClick((-4, 0))
+    print(
+        f"Solve time: {solveTime}, solve {nrSolve}, Average = {avg}, Standard deviation = {std}, Fastest time = {m}")
+    time.sleep(0.5)
+
+    return t
